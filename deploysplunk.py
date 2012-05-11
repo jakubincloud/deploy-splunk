31956ae0fee13146c7138559b73f8eb53bf5031d
Jakub Zygmunt <jakub.zygmunt@cloudreach.co.uk>
2012-05-11 11:33:08 +0100

    Testing manual and external config

--- deploysplunk.py
+++ deploysplunk.py
@@ -1,13 +1,30 @@
 import subprocess
 import sys
-import cirrus_cmdb
+import ConfigParser
+from cirrus_cmdb import CirrusCmdb
 
 __author__ = 'jakub.zygmunt'
 
+class Struct:
+    """
+    convert dictionary to object, thanks to StackOverflow
+    """
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
 class DeploySplunk(object):
-    def __init__(self, config=None, out=sys.stdout):
-        self.config = config
+    def __init__(self, config=None, file=None, out=sys.stdout):
         self.out = out
+
+        self.config = Struct(**config) if isinstance(config, dict) else config
+        if file:
+            self.readConfigFile(file)
+
+        self.is_connected = False
+
+        if self.config:
+            self.cmdb = CirrusCmdb(base_api_url=self.config.base_url, user=self.config.user, password=self.config.password)
+            self.is_connected = self.cmdb.can_connect()
 
     def log(self, msg):
         self.out.write(msg)
@@ -15,8 +32,19 @@ class DeploySplunk(object):
     def testOutput(self, msg):
         self.log(msg)
 
+    def readConfigFile(self, file):
+        parser=ConfigParser.SafeConfigParser()
+        parser.read(file)
+        if 'cmdb' in parser.sections():
+            self.config = Struct(**{f:parser.get('cmdb',f) for f in ('base_url', 'user', 'password')})
+        else:
+            self.log('Invalid config file {0}'.format(file))
+
     def deploy(self, clientName):
         if self.config:
-            pass
+            if self.is_connected:
+                pass
+            else:
+                self.log('Cannot connect to cmdb.')
         else:
             self.log('No config found')
--- deploysplunk_test.py
+++ deploysplunk_test.py
@@ -11,18 +11,49 @@ class DeploySplunkTest(unittest.TestCase):
         """
         self.out = StringIO()
 
+    def getOutput(self):
+        return self.out.getvalue().strip()
+
     def testRedirectOutput(self):
         expectedString = 'This is a logging test'
         ds = DeploySplunk(out=self.out)
         ds.testOutput(expectedString)
-        output = self.out.getvalue().strip()
-        self.assertTrue(expectedString, output)
+        output = self.getOutput()
+        self.assertEquals(expectedString, output)
 
     def testEmptyConstructor(self):
         expectedString = 'No config found'
         ds = DeploySplunk(out=self.out)
         ds.deploy('client')
-        output = self.out.getvalue().strip()
-        self.assertTrue(expectedString, output)
+        output = self.getOutput()
+        self.assertEquals(expectedString, output)
+
+    def testConstructorInvalidConfig(self):
+        expectedString = 'Cannot connect to CMDB'
+        config = {  'base_url': 'http://127.0.0.1',
+                    'user': 'a',
+                    'password': 'b',
+        }
+
+        ds = DeploySplunk(config=config, out=self.out)
+        self.assertFalse(ds.is_connected)
+    def testConstructorInvalidFileConfigNoCMDBSection(self):
+            configFile = 'credentials/.invalid_cmdb1'
+            expectedString = 'Invalid config file {0}'.format(configFile)
+            ds = DeploySplunk(file=configFile, out=self.out)
+            output = self.getOutput()
+            self.assertEquals(expectedString, output)
+            self.assertFalse(ds.is_connected)
+
+    def testConstructorInvalidFileConfigInvalidCredentials(self):
+        expectedString = 'Cannot connect to cmdb.'
+        ds = DeploySplunk(file='credentials/.invalid_cmdb', out=self.out)
+        ds.deploy('client');
+        output = self.getOutput()
+        self.assertEquals(expectedString, output)
+
+    def testConstructorValidFileConfig(self):
+        ds = DeploySplunk(file='credentials/.valid_cmdb', out=self.out)
+        self.assertTrue(ds.is_connected)
 
 
